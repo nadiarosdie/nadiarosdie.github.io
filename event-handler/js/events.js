81f68d4a7d94bddd49e5a7a5b0b909ff12b7c982
nadiarosdie <nadiarosdie@gmail.com>
2021-10-01 21:03:52 +0800

    Password Input

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,7 +2,25 @@ function transformUpperCase (_val) {
     return _val.toUpperCase();
 }
 
+
+//DOM event listener
 let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUpperCase(elNamaPengguna.value)
 })
+
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+
+    if(pass_val.length > 5) {
+        alert("Password check....OK!")
+    } else {
+        alert("Password is too short!")
+    }
+}
+
+//traditional DOM event handler
+let elKL = document.getElementById("kataLaluan") 
+elKL.onblur = checkPassword
