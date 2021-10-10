70b75dae18e2d2f09defab8f93e9aebbe5fcb879
nadiarosdie <nadiarosdie@gmail.com>
2021-10-11 04:56:29 +0800

    Correction: define lon on haversine files

--- geolocation/js/haversine.js
+++ geolocation/js/haversine.js
@@ -1,11 +1,11 @@
 function haversine(lat1, lon1, lat2, lon2) {
     var R = 6371; //earth radius in KM
     var dLat = (lat2 - lat1).toRad();
-    var dLong = (lon2 -    lon1).toRad();
+    var dLon = (lon2 -    lon1).toRad();
     var lat1 = lat1.toRad();
     var lat2 = lat2.toRad();
 
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c; // Kilometers
 
--- geolocation/js/haversine_main.js
+++ geolocation/js/haversine_main.js
@@ -1,4 +1,4 @@
-function calculateDistances(lat,long) {
+function calculateDistances(lat,lon) {
     let dpulze = haversine(lat,lon,2.922561,101.650965)
     let pyramid = haversine(lat,lon,3.073065,101.607787)
     let klcc = haversine(lat,lon,3.158761,101.714524)
