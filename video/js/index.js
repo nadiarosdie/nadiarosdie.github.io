b606ebecc16fe0a78927bf104356b8586a4d66dd
nadiarosdie <nadiarosdie@gmail.com>
2021-10-11 05:17:01 +0800

    video -seekbar and volume control

--- video/js/index.js
+++ video/js/index.js
@@ -21,3 +21,21 @@ freezeBtn.addEventListener("mousedown",function(){
 freezeBtn.addEventListener("mouseup",function(){
     video.play()
 })
+
+
+let seekBar = document.getElementById("seekBar")
+seekBar.addEventListener("change", function(){
+    let time = video.duration * (seekBar.value/100)
+    video.currentTime = time
+})
+
+video.addEventListener("timeupdate", function(){
+    let value = (100/video.duration) * video.currentTime
+    seekBar.value = value
+})
+
+
+let volumeControl = document.getElementById("volume")
+volumeControl.addEventListener("change", function(){
+    video.volume = volumeControl.value
+})
