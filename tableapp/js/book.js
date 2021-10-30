8c90bb05693eb4402bc1946a1d8808eade1704d4
nadiarosdie <nadiarosdie@gmail.com>
2021-10-31 01:32:45 +0800

    tableabookingpp

---
+++ tableapp/js/book.js
@@ -0,0 +1,24 @@
+let bookNowBtn = document.getElementById("bookNow")
+bookNowBtn.addEventListener("click", function(){
+    let userName = document.getElementById("userName")
+    let userNameVal = userName.value
+
+    let userEmail = document.getElementById("userEmail")
+    let userEmailVal = userEmail.value
+
+    let userPax = document.getElementById("userPax")
+    let userPaxVal = userPax.value
+
+    let userRemarks = document.getElementById("userRemarks")
+    let userRemarksVal = userRemarks.value
+
+    bookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal)
+})
+
+function bookNow(userName, userEmail, userPax, userRemarks){
+    console.log(userName)
+    console.log(userEmail)
+    console.log(userPax)
+    console.log(userRemarks)
+}
+    
