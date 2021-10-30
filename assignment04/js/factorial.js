cc09d85d9048bb2ba3ca5cb0d7a3dae2b69f6d9a
nadiarosdie <nadiarosdie@gmail.com>
2021-10-31 04:21:33 +0800

    Factorial Assignment 04

---
+++ assignment04/js/factorial.js
@@ -0,0 +1,22 @@
+let calFactBtn = document.getElementById("calFact")
+calFactBtn.addEventListener("click", function () {
+    //console.log("calculate button clicked!")
+    let number = document.getElementById("factNum").value    
+
+    calculateFact(number)
+})
+
+function calculateFact(number){
+    let f = 1;
+
+    if (number<0) {
+        alert("Please input positive integer!") 
+    } else if (number==0) {
+        alert("Factorial for " + number + " = 1") 
+    } else {
+        for (let i=1; i<=number; i++){
+            f *= i;}
+        alert("Factorial for " + number + " = " + f)
+    }
+}
+
